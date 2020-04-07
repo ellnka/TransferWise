5e5c756fc1b5abc7637a4b8783f399b3dd51249f
Elena Y <alenkasmail@gmail.com>
2020-04-07 17:33:18 +0300

    feat: new sections added

--- js/banner.js
+++ js/banner.js
@@ -3,7 +3,7 @@
 /*** banner ***/
 
 const $banner = document.querySelector(".banner");
-const $bannerBtnClose = $banner.querySelector(".button-close");
+const $bannerBtnClose = $banner.querySelector(".banner__close-icon");
 
 $bannerBtnClose.onclick = () => {
     $banner.style.display = "none";
